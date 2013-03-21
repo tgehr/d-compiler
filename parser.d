66c50daed59585260a7252e8e38e8a3f2c95e3dd
Timon Gehr <timon.gehr@gmx.ch>
2013-03-21 15:55:19 +0100

    Initial commits.

--- parser.d
+++ parser.d
@@ -1,15 +1,34 @@
 module parser;
 
-import std.array, std.range, std.algorithm, std.traits;
+import std.array, std.range, std.algorithm, std.traits, std.conv: to;
 
 import lexer;
+
+template numClasses(T...){
+	static if(T.length == 0) enum numClasses = 0;
+	else enum numClasses = is(T[0] == class) + numClasses!(T[1..$]);
+}
+
+private template finalParse(string name,int p){
+	enum finalParse = {string r=name~"(";foreach(t;0..p) r~="_"~to!string(t)~","; return r~");\n";};
+}
+
+private template getParseImpl(string name,int p,T...){
+	static if(T.length == 0) enum getParseImpl = "return "~finalParse!(name,p)();
+	else static if(is(typeof(T[0]==string))) enum getParseImpl = "expect(Tok!\""~T[0]~"\");\n" ~ getParseImpl!(name,p,T[1..$]);
+	else static if(is(T[0] == class)) enum getParseImpl = "auto _"~to!string(p)~"="~T[0].parse~";\n"~getParseImpl!(name,p+1,T[1..$]);
+	else enum getParse = "parse"~T[0].stringof~"()";
+}
+
+mixin template getParse(T...){enum parse = "{\n"~getParseImpl!(this.stringof,0,T)~"}()";}
 
 class Expression{
 	Location loc;
 	this(){}
 	override string toString(){assert(0,"unknown expression.");}
+	enum parse = "parseExpression()";
 }
-class Statement{}
+class Statement{enum parse = "parseStatement()";}
 class DefinitionStm: Statement{}
 class TypeAST{}
 
@@ -73,7 +92,10 @@ class ExpressionStm: Statement{
 	override string toString(){return e.toString() ~ ';';}
 }
 
-class IfStm : Statement{}
+class IfStm : Statement{
+	mixin getParse!("if","(",Expression,")",Statement,"<?","else",Statement,"?>");
+	pragma(msg,parse);
+}
 class ForStm : Statement{}
 class WhileStm : Statement{}
 class DoWhileStm : Statement{}
@@ -164,10 +186,103 @@ template matchingDelimiter(TokenType left) if(isLeftDelimiter){
 		}
 	}();
 }
-private template isCode(R){enum isCode=isForwardRange!R && is(Unqual!(ElementType!R) == Token);}
+//private template isCode(R){enum isCode=isForwardRange!R && is(Unqual!(ElementType!R) == Token);}
+
+alias immutable(Token)[] Code;
+
+private struct Parser{
+	enum filename = "tt.d";
+	Code code;
+	Token tok;
+	Location loc;
+	this(Code code){
+		this.code = code;
+		this.loc = Location(filename,1);
+		nextToken();
+	}
+	void nextToken(){
+		tryagain:
+		if(code.empty) return tok=token!"EOF";
+		tok = code.front();
+		code.popFront();
+		if(tok.type==Tok!"\n"){loc.line++; goto tryagain;}
+		else if(tok.type==Tok!"Error"){loc.error(tok.str); goto tryagain;}
+	}
+	static class ParseErrorException: Exception{this(string s){super(s);}} alias ParseErrorException PEE;
+	void error(string msg){loc.error(msg);}
+	void expect(TokenType type){
+		if(tok.type!=type) error("found '" ~ tok.toString() ~ "' when expecting '" ~ Token(type).toString() ~"'");
+		else nextToken();
+	}
+	auto dp(alias a, T...)(T args,ref Token t){ // dynamic dispatch based on token type
+		final switch(t.type){
+			mixin({
+				string r;
+				foreach(t;__traits(allMembers, TokenType)) r~=`case TokenType.` ~ t ~ `:  return a!(TokenType.` ~ t ~ `)(args,t);`;
+				return r;
+			}());
+		}
+	}
+	// Operator precedence expression parser
+	// null denotation
+	Expression nud(TokenType type)(ref Token self) {
+		static if(type==Tok!"i") return new IdentifierExp(self.name);
+		else static if(type == Tok!"__error") return new ErrorExp();
+		else static if(isLiteral!type) return new LiteralExp(self);
+		else static if(type==Tok!"("){
+			auto e=parseExpression(0);
+			expect(Tok!")");
+			return e;
+		}else static if(isUnaryOp!type) return new UnaryExp!type(parseExpression(nbp));
+		else throw new PEE("invalid unary operator '"~tok.toString()~"'");
+	}
+	// left denotation
+	Expression led(TokenType type)(Expression left, ref Token self){
+		static if(type == Tok!"?"){
+			auto e=parseExpression(0);
+			expect(Tok!":");
+			return new TernaryExp(left,e,parseExpression(0));
+		}else static if(type == Tok!"["){auto e=new IndexExp(left,parseExpression(0)); expect(Tok!"]"); return e;}
+		else static if(type == Tok!"("){auto e=new CallExp(left,parseExpression(0)); expect(Tok!")"); return e;} //TODO: Merge with preceding
+		else static if(isBinaryOp!type) return new BinaryExp!type(left,parseExpression(.rbp!type));
+		else static if(isPostfixOp!type) return new PostfixExp!type(left);
+		else throw new PEE("invalid binary operator '"~TokChars!type~"'");
+	}
+	Expression parseExpression(int rbp = 0){
+		retry:
+		auto t = tok;
+		nextToken();
+		Expression left;
+		try left = dp!nud(t); catch(PEE err){error(err.msg); if(tok.type==Tok!"EOF") return new ErrorExp(); goto retry;}
+		while(rbp < getLbp(tok.type)){ // TODO: replace with array lookup
+			t = tok;
+			nextToken();
+			try left = dp!led(left,t); catch(PEE err){error(err.msg);}
+		}
+		return left;
+	}
+	Statement parseStatement(){
+		switch(tok.type){
+			case Tok!"if":
+				nextToken();
+				expect(Tok!"(");
+				Expression e = parseExpression(0);
+				expect(Tok!")");
+				//if(tok.type==Tok!";") error
+			default:
+				error("found '" ~ tok.toString() ~ "' instead of statement");
+				return null; // error?
+		}
+	}
+	auto parse(){return parseExpression();}
+}
+
+auto parse(Code code){
+	return Parser(code).parse();
+}
 
 import std.stdio;
-auto parse(R)(R code) if(isCode!R) { // Token range
+/*auto parseo(Code code) {
 	if(code.empty) throw new Exception("code is empty");
 	enum filename = "tt.d";
 	auto loc = Location(filename,1);
@@ -243,7 +358,7 @@ auto parse(R)(R code) if(isCode!R) { // Token range
 	}
  	parseExpressionDG = &parseExpression;
 	return parseExpression();//parseStatement();
-}
+}*/
 
 
 
