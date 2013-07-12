349f633f53290987712191a16ff17db0b14909dc
Timon Gehr <timon.gehr@gmx.ch>
2013-07-12 06:53:41 +0200

    Tests for ambiguity detection in presence of imports.

--- test/mixin_.d
+++ test/mixin_.d
@@ -1,13 +1,32 @@
+mixin template Ambig(immutable(char)[] x){
+	mixin("enum "~x~"=1;"); // error
+}
 
-mixin template Attribs(){ int foo(int x){ return x+y; } }
+static if(!is(typeof(aax))) mixin Ambig!"bbx";
+static if(!is(typeof(bbx))) mixin Ambig!"aax";
+
+struct InexistentIdentNested{
+	mixin template InexistentIdent(){ enum xx=y; } // error
+	mixin InexistentIdent;
+}
+
+mixin template Attribs(bool hasy){
+	int foo(int x){
+		static if(hasy) return x+y; // error
+		else return x;
+	}
+}
 
 struct AttribS{
 	int y;
-	pure static mixin Attribs;
+	pure static mixin Attribs!false;
 	static assert(is(typeof(&foo)==int function(int)pure));
 	struct X{
-		mixin Attribs;
+		mixin Attribs!false;
 		static assert(is(typeof(&foo)==int delegate(int)));
+		struct Y{
+			static mixin Attribs!true;
+		}
 	}
 }
 
@@ -37,13 +56,6 @@ struct AConfl{
 	mixin NConfl;
 	pragma(msg, conflFoo(2)); // error
 }
-
-mixin template Ambig(immutable(char)[] x){
-	mixin("enum "~x~"=1;"); // TODO: error
-}
-
-static if(!is(typeof(aax))) mixin Ambig!"bbx";
-static if(!is(typeof(bbx))) mixin Ambig!"aax";
 
 mixin template FooZ(){
 	int foo(){ return 1; }
@@ -82,9 +94,14 @@ void fun(){
 }
 
 struct Fun{
+	mixin template Bar(int x){
+		enum Bar = "Don't do eponymous lookup!";
+		enum foo = y+2;
+		pragma(msg, foo);
+	}
 	enum y=3;
 	mixin Bar!2;
-	pragma(msg, Bar);
+	pragma(msg, Bar); // error
 	static assert(foo==5);
 }
 
